2fbc1a80fb799be776d2ec5b26105bedd9c8975f
jxmot <webdev@stupiddog.net>
2022-04-26 07:15:47 -0500

    minor change

--- public_html/gfscripts/getghdata-cron.sh
+++ public_html/gfscripts/getghdata-cron.sh
@@ -18,3 +18,8 @@ curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/"
 curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/"$owner"/repos?type=sources&sort=updated&per_page=100" > "./"$owner"repos.json" 2>/dev/null
 curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/"$owner"/events" > "./"$owner"events.json" 2>/dev/null
 curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/"$owner"/gists" > "./"$owner"gists.json" 2>/dev/null
+# This is for checking the rate limits, 
+# look at its contents periodocally to 
+# verify all is well for the chosen CRON 
+# interval.
+curl -H "Accept: application/vnd.github.v3+json" -I "https://api.github.com/users/"$owner 2>/dev/null | grep --ignore-case "^x-ratelimit" > ./x-ratelimit.log
