adfea36a56f9b25cd7093710e271666c947a79ac
jxmot <webdev@stupiddog.net>
2022-04-26 06:49:51 -0500

    added conditional folder creation

--- public_html/gfscripts/getghdata-cron.sh
+++ public_html/gfscripts/getghdata-cron.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 # This is the CRON version of this file.
 owner="jxmot"
-gfdata=$HOME/public_html/gfdata
 
+ghfeeds=$HOME/public_html/ghfeeds
+if [[ ! -d $ghfeeds ]];then
+    mkdir $ghfeeds
+fi
+cd $ghfeeds
+
+gfdata=$HOME/public_html/ghfeeds/gfdata
 if [[ ! -d $gfdata ]];then
     mkdir $gfdata
 fi
-
 cd $gfdata
+
 curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/"$owner > "./"$owner"user.json" 2>/dev/null
 curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/"$owner"/repos?type=sources&sort=updated&per_page=100" > "./"$owner"repos.json" 2>/dev/null
 curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/"$owner"/events" > "./"$owner"events.json" 2>/dev/null
