cea3b30525688cb188c58bcad0ce30083d57f159
jxmot <webdev@stupiddog.net>
2022-04-27 15:17:16 -0500

    improved shell scripts

--- public_html/gfscripts/getghdata-cron.sh
+++ public_html/gfscripts/getghdata-cron.sh
@@ -3,32 +3,20 @@
 # An interval of 5 to ?? minutes is 
 # recommended, adjust as needed.
 #
-# GitHub repository owner
+# GitHub repository owner. Edit as needed.
 owner="jxmot"
-# This is here just in case this script 
-# is ran before uploading the project 
-# to your server. Edit as needed.
-ghfeeds=$HOME/public_html/ghfeeds
-if [[ ! -d $ghfeeds ]];then
-    mkdir $ghfeeds
-fi
-cd $ghfeeds
-# The data will be written here, so 
-# create the folder if needed. 
 # Edit as needed.
-gfdata=$HOME/public_html/ghfeeds/gfdata
-if [[ ! -d $gfdata ]];then
-    mkdir $gfdata
-fi
-cd $gfdata
+docroot="$HOME/public_html"
+# Edit as needed.
+gfdata="$docroot/ghfeeds/gfdata"
 # Get the GitHub data and save it to 
 # JSON files.
-curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/"$owner > "./"$owner"user.json" 2>/dev/null
-curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/"$owner"/repos?type=sources&sort=updated&per_page=100" > "./"$owner"repos.json" 2>/dev/null
-curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/"$owner"/events" > "./"$owner"events.json" 2>/dev/null
-curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/"$owner"/gists" > "./"$owner"gists.json" 2>/dev/null
+curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/$owner" > $gfdata/$owner"user.json" 2>/dev/null
+curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/$owner/repos?type=sources&sort=updated&per_page=100" > $gfdata/$owner"repos.json" 2>/dev/null
+curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/$owner/events" > $gfdata/$owner"events.json" 2>/dev/null
+curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/$owner/gists" > $gfdata/$owner"gists.json" 2>/dev/null
 # This is for checking the rate limits, 
 # look at its contents periodocally to 
 # verify all is well for the chosen CRON 
 # interval.
-curl -H "Accept: application/vnd.github.v3+json" -I "https://api.github.com/users/"$owner 2>/dev/null | grep --ignore-case "^x-ratelimit" > ./x-ratelimit.log
+curl -H "Accept: application/vnd.github.v3+json" -I "https://api.github.com/users/$owner" 2>/dev/null | grep --ignore-case "^x-ratelimit" > $gfdata/x-ratelimit.log
--- public_html/gfscripts/getghdata.sh
+++ public_html/gfscripts/getghdata.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # This is the command line version of this file. Do not
-# run it from CRON.
+# run it from CRON. Change the current folder to where 
+# you want the data files to go. Then run this script 
+# using a relative or absolute path to its location.
 if [ -z "$1" ];then
     echo "Missing repository owner ID!"
     echo "Usage: getghdata.sh GitHubUser"
@@ -14,12 +16,12 @@ owner=$1
 # Get the GitHub data and save it to 
 # JSON files.
 echo $owner"user.json"
-curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/"$owner > "./"$owner"user.json" 2>/dev/null
+curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/$owner" > "./"$owner"user.json" 2>/dev/null
 echo $owner"repos.json"
-curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/"$owner"/repos?type=sources&sort=updated&per_page=100" > "./"$owner"repos.json" 2>/dev/null
+curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/$owner/repos?type=sources&sort=updated&per_page=100" > "./"$owner"repos.json" 2>/dev/null
 echo $owner"events.json"
-curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/"$owner"/events" > "./"$owner"events.json" 2>/dev/null
+curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/$owner/events" > "./"$owner"events.json" 2>/dev/null
 echo $owner"gists.json"
-curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/"$owner"/gists" > "./"$owner"gists.json" 2>/dev/null
+curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/$owner/gists" > "./"$owner"gists.json" 2>/dev/null
 echo "Rate Limit After:"
-curl -H "Accept: application/vnd.github.v3+json" -I "https://api.github.com/users/"$owner 2>/dev/null | grep --ignore-case "^x-ratelimit"
+curl -H "Accept: application/vnd.github.v3+json" -I "https://api.github.com/users/$owner" 2>/dev/null | grep --ignore-case "^x-ratelimit"
