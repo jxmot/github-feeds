963b0db52df399807cc31c5b30a789ba8165222e
jxmot <webdev@stupiddog.net>
2022-05-01 12:22:16 -0500

    clean up, added comments

--- public_html/assets/js/utils.js
+++ public_html/assets/js/utils.js
@@ -1,5 +1,11 @@
+/* 
+    Utility Functions
 
+    Author: https://github.com/jxmot
+    Repository: https://github.com/jxmot/github-feeds
+*/
 
+// insert a new string into a string at an index
 function insertAt(orig, index, insrt) {
     if(index > 0) {
         return orig.substring(0, index) + insrt + orig.substr(index);
@@ -7,6 +13,7 @@ function insertAt(orig, index, insrt) {
     return insrt + orig;
 };
 
+// replace a character with a new string at an index
 function replaceAt(orig, index, rplcmnt) {
     if(index >= orig.length) {
         return orig.valueOf();
@@ -14,10 +21,12 @@ function replaceAt(orig, index, rplcmnt) {
     return orig.substring(0, index) + rplcmnt + orig.substring(index + 1);
 };
 
+// is the number even?
 function isEven(num) {
     return ((num % 2) === 0);
 };
 
+// render markdown flavored text as HTML
 function renderMD(input) {
     var step = 0;
     var loc = 0;
@@ -25,18 +34,16 @@ function renderMD(input) {
     while((loc = output.lastIndexOf('`')) !== -1) {
         output = replaceAt(output, loc, '<');
         if(isEven(step)) {
-            //output = insertAt(output, loc+1, '/pre>');
             output = insertAt(output, loc+1, '/code>');
         } else {
-            //output = insertAt(output, loc+1, 'pre style="display:inline;">');
             output = insertAt(output, loc+1, 'code>');
         }
         step++;
     }
-    //return '<p>' + output + '</p>';
     return output;
 };
 
+// Used for testing, uncomment and run with node.js
 //var input = "notes:\n* each entry save `d[i].created_at` in an attribute of the `<p class=\"date\">`\n* to iterate through entries it may be easier to add & use a sequenced ID - `<p id=\"ago-[1 - x]\" class=\"date\">`";
 //var input = "Tried a couple of fontawesome SVGs, did not work well. And they do not (*this is expected*) size easily. Decided that HTML codes (&#9788; & &#9789;) would work better. They are easier to size via CSS and the colors can be easily changed.\n\nInstead of changing over to the icons a means to select icons or emojis was added. Look for `lightdarkicon` and change to either true(icons) or false(emojis).";
 //console.log(renderMD(input));
