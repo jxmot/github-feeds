eddc53353e0a2039a032e7d3a7441a410a08244c
jxmot <webdev@stupiddog.net>
2022-06-16 10:15:41 -0500

    do not render commit line if length is 0

--- public_html/assets/js/github-feed.js
+++ public_html/assets/js/github-feed.js
@@ -594,16 +594,18 @@ waitforit = true
                     e += '        <a href="https://github.com/' + d[i].repo.name + '" target="_blank">' + d[i].repo.name + '</a>';
                     var c = d[i].payload.commits.length;
 
-                    if (c === 2) {
+                    if(c === 2) {
                         e += '    <p><a class="shalink" href="https://github.com/' + d[i].repo.name + '/commit/' + d[i].payload.commits[0].sha + '" target="_blank">' + d[i].payload.commits[0].sha.substr(0, 6) + '</a> - ' + renderMD(d[i].payload.commits[0].message, {simpleLineBreaks: true, _noPwrap: true}) + '</p>';
                         e += '    <p><a class="shalink" href="https://github.com/' + d[i].repo.name + '/commit/' + d[i].payload.commits[1].sha + '" target="_blank">' + d[i].payload.commits[1].sha.substr(0, 6) + '</a> - ' + renderMD(d[i].payload.commits[1].message, {simpleLineBreaks: true, _noPwrap: true}) + '</p>';
                         e += '    <br><p><a href="https://github.com/' + d[i].repo.name + '/compare/' + d[i].payload.before + '...' + d[i].payload.head + '" target="_blank">View comparison for these 2 commits &raquo;</a></p>'
-                    } else if (c > 2) {
+                    } else if(c > 2) {
                         e += '    <p><a class="shalink" href="https://github.com/' + d[i].repo.name + '/commit/' + d[i].payload.commits[0].sha + '" target="_blank">' + d[i].payload.commits[0].sha.substr(0, 6) + '</a> - ' + renderMD(d[i].payload.commits[0].message, {simpleLineBreaks: true, _noPwrap: true}) + '</p>';
                         e += '    <p><a class="shalink" href="https://github.com/' + d[i].repo.name + '/commit/' + d[i].payload.commits[1].sha + '" target="_blank">' + d[i].payload.commits[1].sha.substr(0, 6) + '</a> - ' + renderMD(d[i].payload.commits[1].message, {simpleLineBreaks: true, _noPwrap: true}) + '</p>';
                         e += '    <br><p><a href="https://github.com/' + d[i].repo.name + '/compare/' + d[i].payload.before + '...' + d[i].payload.head + '" target="_blank">' + (c - 2) + ' more commit &raquo;</a></p>'
                     } else {
-                        e += '    <p><a class="shalink" href="https://github.com/' + d[i].repo.name + '/commit/' + d[i].payload.commits[0].sha + '" target="_blank">' + d[i].payload.commits[0].sha.substr(0, 6) + '</a> - ' + renderMD(d[i].payload.commits[0].message, {simpleLineBreaks: true, _noPwrap: true}) + '</p>';
+                        if(c > 0) {
+                            e += '    <p><a class="shalink" href="https://github.com/' + d[i].repo.name + '/commit/' + d[i].payload.commits[0].sha + '" target="_blank">' + d[i].payload.commits[0].sha.substr(0, 6) + '</a> - ' + renderMD(d[i].payload.commits[0].message, {simpleLineBreaks: true, _noPwrap: true}) + '</p>';
+                        }
                     }
                     e += '    </div>';
                     e += '</div>'
